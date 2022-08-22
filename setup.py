ed5bce0721a592a2b0522f7042df1ef370d915b5
jameshskelton <jameshskelton@gmail.com>
2022-08-22 16:29:39 -0400

    updated to run with DALL-E Mini Gradient

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
+    long_description = f.read()
 
 setuptools.setup(
     name='vqgan-jax',
@@ -15,5 +15,5 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
-    install_requires=['jax>=0.2.6', 'flax', 'transformers'],
+    install_requires=['transformers'],
 )
